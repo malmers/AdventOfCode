18bf1c1d4c3b86d664bd2d45db7a31f1948b7436
Pontus Malm <malm@chalmers.it>
2016-12-06 13:50:39 +0100

    Day six, part two

---
+++ day06/part2.rb
@@ -0,0 +1,20 @@
+# Advent of Code - Day six
+
+class Advent
+
+  def initialize
+    b = 0
+    input = File.read('input')
+    input = input.split("\n")
+    input.map! { |e| e.each_char.to_a }
+    input = input.transpose
+    input.each do |row|
+      u = row.uniq
+      print u.min_by { |a| row.count a }
+    end
+  end
+end
+
+puts "Code:"
+Advent.new
+print "\n"
