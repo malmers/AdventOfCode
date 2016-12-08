2388fc4170c3cd4ed666b2707c08eca2a3c09d82
Pontus Malm <malm@chalmers.it>
2016-12-08 23:29:01 +0100

    Day seven, part one

---
+++ day07/part1.rb
@@ -0,0 +1,33 @@
+# Advent of Code - Day six
+
+class Advent
+
+  def contain_abba?(string)
+    return false if string.size < 4
+    if string[0] == string[3] and string[1] == string[2] and string[0] != string[1]
+      return true
+    end
+    return contain_abba? string[1..-1]
+  end
+
+  def initialize
+    input = File.read('input')
+    input = input.split("\n")
+    input.select! do |row|
+      nonets = row
+      hypernets = row.scan(/\[(\w*)\]/)
+      hypernets.map! { |e| e.first }
+      hypernets.each do |hyper|
+        nonets = nonets.gsub("[#{hyper}]", '.')
+      end
+      nonets = nonets.split '.'
+
+      nonets.any? { |e| contain_abba? e } and
+      hypernets.none? { |e| contain_abba? e }
+    end
+    puts "#{input.size}"
+  end
+end
+
+puts "Code:"
+Advent.new
